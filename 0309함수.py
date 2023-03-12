4e58667fa23d92ea6c82c34a603bfc882beb9fbe
KEN <shdnrnjs@gmail.com>
2023-03-12 22:47:17 +0900

    dictionary, set, function review

--- 0309함수.py
+++ 0309함수.py
@@ -1,7 +1,7 @@
 #함수
 #연습문제
 #1. 1~n까지의 합을 계산하는 함수
-
+"""
 def sum(n):
     sum = 0
     for i in range(0,n+1):
@@ -9,10 +9,11 @@ def sum(n):
     return sum
     
 print(sum(10))
+"""
 
 #반지름을 전달하면 원의 면적을 반환하는 cir_area(r) 함수와 원의 둘레를 반환하는 cir_cirm(r) 함수를 작성.
 #이들 함수를 이용하여 반지름이 3.5cm인 원의 면적과 둘레를 소수점 아래 첫 자리까지 구하라. --다시할 것
-
+"""
 def cir_area(r):
     return 3.14*r**2
     
@@ -22,7 +23,9 @@ def cir_cirm(r):
 
 print(round(cir_area(3.5),1))
 print(round(cir_cirm(3.5),1))
+"""
 
+"""
 #den(n) 함수를 이용하여 약수를 구하여 반환하는 프로그램을 작성 --다시할 것
 def den(n):
     cd=[]
@@ -31,7 +34,20 @@ def den(n):
             cd.append(i)
     return cd
 print(den(12))
+"""
 
+"""나의 코드
+def den(n):
+    list1=[]
+    for x in range(1, n+1):
+        if n % x == 0:
+            list1.insert(0, x)
+    return list1
+print(sorted(den(12)))
+"""
+
+
+"""
 #함수 인자 전달 방식
 # 인자의 순서와 조합 사용 예시
 def print_numbers(a, b, *args, c=10, d=20):
@@ -44,7 +60,9 @@ def print_numbers(a, b, *args, c=10, d=20):
 # 함수 호출
 print_numbers(1, 2, 3, 4, 5, c=30, d=40)
 # 출력 결과: a: 1, b: 2, c: 30, d: 40, args: (3, 4, 5) args가 투플 형태로 출력된다
+"""
 
+"""
 #지역변수 전역변수 
 x = 2  # 전역 변수
 
@@ -56,8 +74,9 @@ def my_function():
 
 my_function()
 print('x:', x)
+"""
 
-
+"""
 #지역변수와 전역번수 - global
 x = 'global'
 
@@ -67,31 +86,36 @@ def my_function():
 
 my_function()
 print(x)
+"""
 
+"""
 ##람다함수## - 가독성 높여줌 
 # 기존 함수 정의 방법
 def add(a, b):
     return a + b
 
 # 람다 함수 정의 방법
-lambda_add = lambda a, b: a + b
+lambda_add = lambda a, b: a + b # 함수명 = lambda 매개변수명: 리턴값 
 
 # 함수 호출
 result1 = add(3, 4)             # 7
 result2 = lambda_add(3, 4)      # 7
+"""
 
+"""
 #함수의 인자로 전달된 예시, +반환값으로 사용할 수 도 있다.
-students = [
+students = [ #리스트 
     {'name': 'Alice', 'score': 80},
     {'name': 'Bob', 'score': 90},
     {'name': 'Charlie', 'score': 70},
 ]
-
 # 점수(score)를 기준으로 학생 리스트 정렬
-sorted_students = sorted(students, key=lambda student: student['score'], reverse=True)
+sorted_students = sorted(students, key=lambda student: student['score'], reverse=True) # key 매개변수를 람다함수로 설정한다. 학생 딕셔너리 가져와서 score 키값 반환. reverse=True는 내림차순 정렬
 print(sorted_students)
 # 출력 결과: [{'name': 'Bob', 'score': 90}, {'name': 'Alice', 'score': 80}, {'name': 'Charlie', 'score': 70}]
+"""
 
+"""
 #내부함수
 def outer_func(x):
     def inner_func(y):
@@ -99,8 +123,10 @@ def outer_func(x):
     return inner_func #함수를 리턴
 
 result = outer_func(10)
-print(result(20))  # 출력결과: 30 -- 다시 이해할 것
+print(result(20))  # 출력결과: 30 -- 다시 이해할 것. 처음에 outer_func(10)호출해서 x에 10 저장하고 inner_func리턴(10+y가 들어있음) . 그려면 result에 inner_func들어있음. 따라서 print(result(20)하면 inner_func에 20 전달하는 거임  .
+"""
 
+"""
 #내부 함수를 이용하여 간단한 계산기 함수를 구현한 예제
 def calculator():
     def add(a, b):
@@ -117,13 +143,15 @@ def calculator():
 
     return add, subtract, multiply, divide # 튜플(소괄호 생략)
 
-add, subtract, multiply, divide = calculator() # 튜플을 언패킹 
-
+add, subtract, multiply, divide = calculator() # 튜플을 언패킹. 함수를 사용하려면 튜플을 언패킹해야한다. 튜플의 압축을 푸는 것은 개별 값을 별도의 변수에 할당하는 것을 의미
+                                                # 여기서 add, subtract, multiply, divide는 변수임... 함수로 착각했다. 
 print(add(10, 20))  # 출력결과: 30
 print(subtract(10, 20))  # 출력결과: -10
 print(multiply(10, 20))  # 출력결과: 200
 print(divide(10, 20))  # 출력결과: 0.5
+"""
 
+"""
 #코드의 재사용성을 높일 수 있다
 def power(n):
     def inner(x):
@@ -135,7 +163,8 @@ cube = power(3)  # 세제곱 함수를 구현
 
 print(square(3))  # 출력결과: 9
 print(cube(3))  # 출력결과: 27
-
+"""
+"""
 #함수 내 변수 참조 순서
 x = 'global'
 
@@ -151,3 +180,4 @@ def outer():
 
 outer()
 print('x in global:', x)
+"""
--- 0309함수연습문제.py
+++ 0309함수연습문제.py
@@ -1,12 +1,81 @@
 #함수 연습문제
-#1. 두 개의 매개변수 n, m을 전달받아 m x n개의 * 상자를 출력하는 프로그램을 함수로 작성
+#1. 두 개의 매개변수 n, m을 전달받아 m x n개의 '*' 를 출력하는 함수를 작성하라
 #예: 2, 4 -> ****
 #			 ****
+def box(n, m):
+    for i in range(n): #n이 행의 개수
+        print("*" * m) #m은 열의 개수. 줄 바꿈은 자동으로 되네 ? 
+box(2,4)
 
-#2하나의 숫자를 전달받아 숫자의 자리 합을 구하는 함수를 작성예: 123 -> 1+2+3 = 6
 
-#3두 개의 문자열이 서로 다른 처음 위치를 반환하는 함수를 작성. 두 개의 문자열이 같으면 -1을 반환
+#2 하나의 숫자를 전달받아 숫자의 자리 합을 구하는 함수를 작성예: 123 -> 1+2+3 = 6
+"""
+def sum(n):
+    sum = 0;
+    a = int(n/100)
+    a1 = int(n%100)
+    b = int(a1/10)
+    b1 = int(a1%10)
+    
+    if a1 == 0:
+        sum = a
+    elif a1 != 0:
+        sum = a
+        sum += b
+        if b1 != 0:
+            sum += b1
+            
+    return sum
+print(round(sum(541)))
+"""
+
+#3 두 개의 문자열이 서로 다른 처음 위치를 반환하는 함수를 작성. 두 개의 문자열이 같으면 -1을 반환
+"""
+def loc(s1, s2):
+    for i in range(len(s1)): # range로 설정하니까 s1[i] 즉 인덱스 이용할 수 있네?. 그냥 in s1 하니까 사용 안되고.(이건 리스트가 아니라 그런듯) 
+        if s1[i] != s2[i]:
+            return i
+            
+s1 = "abcdefg"
+s2 = "abcdeff"
+
+print(loc(s1,s2))
+"""
+#gpt 코드
+"""
+def find_first_difference(str1, str2):
+    
+    #Finds the first position where two strings differ. Returns -1 if the two strings are equal.
+    
+    for i in range(min(len(str1), len(str2))):
+        if str1[i] != str2[i]:
+            return i
+    if len(str1) != len(str2):
+        return min(len(str1), len(str2))
+    return -1
+"""
+            
 
 #4문자열과 하나의 문자를 전달받아 문자열에서 문자의 위치를 모두 찾아 리스트로 반환하는 함수를 작성
+def location(str, c):
+    list1=[]
+    for i in range(len(str)):
+        if str[i] == c:
+            list1.insert(0, i)
+    return sorted(list1)
+str = "hello"
+c = 'l'
+print(location(str, c))
 
 #5재귀 함수를 이용하여 1부터 100까지의 합을 계산하는 프로그램
+sum=0
+def reculsum(n):
+    if n==1:
+        return 1
+    else:
+        return n + reculsum(n-1)
+print(reculsum(100))
+            
+    
+    
+
